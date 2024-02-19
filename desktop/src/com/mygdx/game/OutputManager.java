3a58e5750ef303a8d2fae8acd2caa09d302a7d92
Taeckun <nicholaslxh97@gmail.com>
2024-02-19 16:55:38 +0800

    we separated the IOManager. Input will be in Playcontrol and output another class

--- desktop/src/com/mygdx/game/OutputManager.java
+++ desktop/src/com/mygdx/game/OutputManager.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class OutputManager {
 
