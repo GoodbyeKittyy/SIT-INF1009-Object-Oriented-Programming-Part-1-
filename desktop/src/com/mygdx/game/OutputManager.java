6c83893aa8411c04b23f3e373311ba6b33f2cbd6
Taeckun <nicholaslxh97@gmail.com>
2024-02-19 16:47:12 +0800

    code for now

---
+++ desktop/src/com/mygdx/game/OutputManager.java
@@ -0,0 +1,54 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+public class OutputManager {
+
+    private Music backgroundMusic;
+    private Sound effectSound;
+    private BitmapFont font;
+    private SpriteBatch spriteBatch;
+
+    public OutputManager() {
+        spriteBatch = new SpriteBatch();
+
+        //Music and Sound
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("background_music.mp3"));
+        effectSound = Gdx.audio.newSound(Gdx.files.internal("effect_sound.wav"));
+    }
+
+    public void playBackgroundMusic(boolean loop) {
+        if (!backgroundMusic.isPlaying()) {
+            backgroundMusic.setLooping(loop);
+            backgroundMusic.play();
+        }
+    }
+
+    public void stopBackgroundMusic() {
+        if (backgroundMusic.isPlaying()) {
+            backgroundMusic.stop();
+        }
+    }
+
+    public void playEffectSound() {
+        effectSound.play();
+    }
+
+    public void drawText(String text, float x, float y) {
+        spriteBatch.begin();
+        font.draw(spriteBatch, text, x, y);
+        spriteBatch.end();
+    }
+
+    public void dispose() {
+        // Dispose of your audio and rendering components when you're done with them
+        if (backgroundMusic != null) backgroundMusic.dispose();
+        if (effectSound != null) effectSound.dispose();
+        if (spriteBatch != null) spriteBatch.dispose();
+        if (font != null) font.dispose();
+    }
+}
